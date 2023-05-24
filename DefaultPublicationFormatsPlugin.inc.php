0a9cc7bc442893da1cef07c51d092ad31e380202
ronste <54445456+ronste@users.noreply.github.com>
2023-05-24 11:08:32 +0200

    Update DefaultPublicationFormatsPlugin.inc.php
    fix downgrade bug

--- DefaultPublicationFormatsPlugin.inc.php
+++ DefaultPublicationFormatsPlugin.inc.php
@@ -24,8 +24,9 @@ class DefaultPublicationFormatsPlugin extends GenericPlugin
      */
     public function register($category, $path, $mainContextId = null)
     {
+	$success = parent::register($category, $path, $mainContextId);
         if (!Config::getVar('general', 'installed') || defined('RUNNING_UPGRADE')) return true;
-        if (parent::register($category, $path, $mainContextId)) {
+        if ($success) {
             if ($this->getEnabled($mainContextId)) {
                 HookRegistry::register('Template::Workflow::Publication', array($this, 'addDefaultPublicationFormats'));
             }
